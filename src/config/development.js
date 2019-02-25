ed88104114ac60a0f968c0d5ad0c02130f35b6f6
DavNej <davnej.dev@gmail.com>
2019-02-25 09:53:13 +0100

    Added config folder

---
+++ src/config/development.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  env: 'development',
+  db: 'mongodb://localhost:27017/museme-scales-app-db',
+  port: process.env.PORT || 3000
+};
---
+++ src/config/test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  env: 'test',
+  db: 'mongodb://localhost:27017/museme-scales-app-db-test',
+  port: process.env.PORT || 3100
+};
