f1f49d21d10f2ee19a13244d500cec755114e6eb
DavNej <davnej.dev@gmail.com>
2019-02-25 20:06:35 +0100

    added test folder

---
+++ src/test/server.test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const request = require('supertest');
+const chai = require('chai');
+const expect = chai.expect;
+
+const server = require('../server');
+
+describe('Test /notes routes', () => {
+  it('Should get all notes', function(done) { 
+    request(server).get('api/notes')
+      .end(function(err, res) {
+        console.log(res)
+        console.log(err)
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.an('array');
+        // expect(res.body).to.be.empty;
+        done();
+      });
+  });
+  // it ('should return all notes!', async () => {
+    // const res = await request(server).get('/ping');
+//     expect(res.status).toBe(200);
+//     // expect(response.text).toBe('Pong!');
+// });
+
+it('should pass', () => {
+    expect(1).to.equal(1);
+  });
+});
