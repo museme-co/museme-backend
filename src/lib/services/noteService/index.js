999dd445ca4f51663645828486670a9b3daf5483
DavNej <davnej.dev@gmail.com>
2019-02-28 14:31:30 +0100

    refact noteService

--- src/lib/services/noteService/index.js
+++ src/lib/services/noteService/index.js
@@ -1,6 +1,23 @@
+'use strict';
+
 const NoteModel = require('./NoteModel');
-const NoteService = require('./NoteService');
 
-const noteService = new NoteService(NoteModel);
+class NoteService {
+  constructor(NoteModel) {
+    this.NoteModel = NoteModel;
+    this.listNotes = this.listNotes.bind(this);
+    this.getNote = this.getNote.bind(this);
+    this.addNote = this.addNote.bind(this);
+  }
+  listNotes(callback) {
+    return this.NoteModel.find({}, callback);
+  }
+  getNote(name, callback) {
+    return this.NoteModel.findOne({ name }, callback);
+  }
+  addNote(note, callback) {
+    return this.NoteModel.create(note, callback);
+  }
+};
 
-module.exports = noteService;
+module.exports = new NoteService(NoteModel);
