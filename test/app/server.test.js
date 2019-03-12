3420ac0a1fc680a92dd6fb4702229d762473ce94
DavNej <davnej.dev@gmail.com>
2019-03-12 13:37:54 +0200

    supertest routes to bee fixed. Commiting for branch management

--- test/app/server.test.js
+++ test/app/server.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const app = require('../../src/app');
+// const app = require('../../src/routes/notes');
 // const config = require('../../src/config/index')
 
 const request = require('supertest');
@@ -8,9 +9,27 @@ const { expect } = require('chai');
 
 describe('Test /notes routes', function() {
   // it('GET api/notes/all', async function(done) { 
+  it('GET api/notes/all', function(done) { 
     
-    // request(app).get('/api/notes/all')
-  //       .end(function(err, res) { 
+    request(app).get('/api/notes/all')
+    // request(app).get('/all')
+    // request(app).get('/foo').then(
+    // request(app).get('/api/notes/all').then(
+    //   (err, res) => {
+   
+    //             console.log('err', err)
+    //             console.log('res', res)
+    //   }
+    // )
+    .expect(200)
+    .end(done);
+  });
+    //           it('GET /all', function(done) { 
+    // request(app).get('/all')
+    //    .expect(200)
+    //     .end(done);
+    //   });
+        // .end(function(err, res) { 
   //         expect(res.statusCode).to.equal(200); 
   //         expect(res.body).to.be.an('array'); 
   //         expect(res.body).to.not.be.empty; 
@@ -18,15 +37,8 @@ describe('Test /notes routes', function() {
   //         // console.log('res', res)
   //         done(); 
   //       }); 
-  // });
-  it ('should return all notes!', async () => {
-    const res = await request(app).get('/api/notes/all')
-    res.expect(res.status).toBe(200);
-    // expect(response.text).toBe('Pong!');
-            done(); 
-});
-
-it('should pass 2', () => {
+  
+  it('should pass 2', () => {
     expect(1).to.equal(1);
   });
   // after()
